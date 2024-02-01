f07fc830e9d7695462e32fedae40e2f270fd186e
beginnerwebprogrammer <ecydenov110@gmail.com>
2024-02-01 17:12:39 +0800

    Updated config for eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,10 +5,18 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'prettier',
   ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'prettier', 'eslint-plugin-react'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
