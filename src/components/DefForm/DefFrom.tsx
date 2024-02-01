4d421f00e7d20080c114dc66b40003b945b049c1
beginnerwebprogrammer <ecydenov110@gmail.com>
2024-02-01 20:53:35 +0800

    Transition from custom form to react-hook-form and refactored registration and login forms

--- src/components/DefForm/DefFrom.tsx
+++ src/components/DefForm/DefFrom.tsx
@@ -1,16 +1,55 @@
-import { FC, FormEventHandler, ReactNode } from 'react';
-import { Box, ThemeProvider } from '@mui/material';
-import { inputTheme } from '../../theme/theme.ts';
+import {
+  Box,
+  Button,
+  FormControl,
+  IconButton,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+  TextField,
+  ThemeProvider,
+} from '@mui/material';
+import { Visibility, VisibilityOff } from '@mui/icons-material';
+import { FormValues } from '../../models/IForm.ts';
+import { inputTheme } from '../../theme/theme';
+import { useForm } from 'react-hook-form';
+import { FC, useState } from 'react';
 
 interface DefFormProps {
-  children: ReactNode;
-  method: FormEventHandler<HTMLFormElement>;
+  method: (data: {
+    email: string;
+    username?: string | undefined;
+    password: string;
+  }) => Promise<void>;
+  name: 'login' | 'registration';
 }
 
-const DefFrom: FC<DefFormProps> = ({ children, method }) => {
+const DefForm: FC<DefFormProps> = ({ method, name }) => {
+  const [showPassword, setShowPassword] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValues>();
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+  };
+
+  const onSubmit = async (data: { email: string; username?: string; password: string }) => {
+    if (name === 'login') {
+      await method({ email: data.email, password: data.password });
+    }
+    if (name === 'registration') {
+      await method({ email: data.email, username: data.username, password: data.password });
+    }
+  };
+
   return (
     <ThemeProvider theme={inputTheme}>
-      <form method='POST' onSubmit={method}>
+      <form method='POST' onSubmit={handleSubmit(onSubmit)}>
         <Box
           sx={{
             display: 'flex',
@@ -21,11 +60,73 @@ const DefFrom: FC<DefFormProps> = ({ children, method }) => {
             padding: '10px 15px',
           }}
         >
-          {children}
+          <TextField
+            {...register('email', { required: true })}
+            id='outlined-basic'
+            label='Email'
+            variant='outlined'
+            required
+            sx={{
+              width: '40ch',
+            }}
+          />
+          {name === 'registration' && (
+            <TextField
+              id='outlined-basic'
+              label='Username'
+              variant='outlined'
+              required
+              sx={{
+                width: '40ch',
+              }}
+              {...register('username', { required: true })}
+            />
+          )}
+          <FormControl
+            sx={{
+              m: 1,
+              width: '40ch',
+              mb: 4,
+            }}
+            variant='outlined'
+            required
+          >
+            <InputLabel htmlFor='outlined-adornment-password'>Password</InputLabel>
+            <OutlinedInput
+              id='outlined-adornment-password'
+              type={showPassword ? 'text' : 'password'}
+              {...register('password', { required: true })}
+              required
+              endAdornment={
+                <InputAdornment position='end'>
+                  <IconButton
+                    aria-label='toggle password visibility'
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                    edge='end'
+                  >
+                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              }
+              label='Password'
+            />
+          </FormControl>
+          <Button
+            type='submit'
+            variant='contained'
+            color='secondary'
+            sx={{
+              width: '47ch',
+              mb: '25px',
+            }}
+          >
+            Login
+          </Button>
         </Box>
       </form>
     </ThemeProvider>
   );
 };
 
-export default DefFrom;
+export default DefForm;
--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,98 +1,16 @@
-import {
-  Button,
-  FormControl,
-  IconButton,
-  InputAdornment,
-  InputLabel,
-  OutlinedInput,
-  TextField,
-} from '@mui/material';
-import { ChangeEvent, FC, FormEvent, useContext, useState } from 'react';
-import { Visibility, VisibilityOff } from '@mui/icons-material';
+import { FormValues } from '../../models/IForm.ts';
 import DefFrom from '../DefForm/DefFrom.tsx';
 import { Context } from '../../main.tsx';
+import { FC, useContext } from 'react';
 
 const LoginForm: FC = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [showPassword, setShowPassword] = useState(false);
   const { store } = useContext(Context);
 
-  const handleClickShowPassword = () => setShowPassword((show) => !show);
-
-  const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
-    event.preventDefault();
-  };
-
-  function handleEmail(e: ChangeEvent<HTMLInputElement>) {
-    setEmail(e.target.value);
+  async function handleSubmit(data: FormValues): Promise<void> {
+    await store.login(data.email, data.password);
   }
 
-  function handlePassword(e: ChangeEvent<HTMLInputElement>): void {
-    setPassword(e.target.value);
-  }
-
-  async function handleSubmit(event: FormEvent<HTMLFormElement>): Promise<void> {
-    event.preventDefault();
-    await store.login(email, password);
-  }
-
-  return (
-    <DefFrom method={handleSubmit}>
-      <TextField
-        id='outlined-basic'
-        label='Email'
-        variant='outlined'
-        required
-        sx={{
-          width: '40ch',
-        }}
-        onChange={handleEmail}
-      />
-      <FormControl
-        sx={{
-          m: 1,
-          width: '40ch',
-          mb: 4,
-        }}
-        variant='outlined'
-        required
-      >
-        <InputLabel htmlFor='outlined-adornment-password'>Password</InputLabel>
-        <OutlinedInput
-          id='outlined-adornment-password'
-          type={showPassword ? 'text' : 'password'}
-          value={password}
-          onChange={handlePassword}
-          required
-          endAdornment={
-            <InputAdornment position='end'>
-              <IconButton
-                aria-label='toggle password visibility'
-                onClick={handleClickShowPassword}
-                onMouseDown={handleMouseDownPassword}
-                edge='end'
-              >
-                {showPassword ? <VisibilityOff /> : <Visibility />}
-              </IconButton>
-            </InputAdornment>
-          }
-          label='Password'
-        />
-      </FormControl>
-      <Button
-        type='submit'
-        variant='contained'
-        color='secondary'
-        sx={{
-          width: '47ch',
-          mb: '25px',
-        }}
-      >
-        Login
-      </Button>
-    </DefFrom>
-  );
+  return <DefFrom method={handleSubmit} name='login' />;
 };
 
 export default LoginForm;
