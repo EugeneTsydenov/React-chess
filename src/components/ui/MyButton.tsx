c1ca46dc393d4e4e929a1c27163009d588c551ee
beginnerwebprogrammer <ecydenov110@gmail.com>
2024-02-09 20:55:25 +0800

    Custom button with MaterialUI

---
+++ src/components/ui/MyButton.tsx
@@ -0,0 +1,27 @@
+import React, { ReactNode, MouseEventHandler } from 'react';
+import { Button } from '@mui/material';
+
+interface ButtonProps {
+  width?: string;
+  height?: string | '50px';
+  children: ReactNode;
+  onClick?: MouseEventHandler<HTMLButtonElement>;
+}
+const MyButton: React.FC<ButtonProps> = ({ children, width, height, onClick }) => {
+  return (
+    <Button
+      type='submit'
+      variant='contained'
+      color='secondary'
+      sx={{
+        width: width,
+        height: height
+      }}
+      onClick={onClick}
+    >
+      {children}
+    </Button>
+  );
+};
+
+export default MyButton;
